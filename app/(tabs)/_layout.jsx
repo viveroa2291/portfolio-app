a6bb67af097b442f7a14dd5a36581c016e20f3be
viveroa2291 <viveroa2291@uwec.edu>
2024-11-03 21:33:52 -0600

    Initial Commit

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,43 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+      }}>
+      <Tabs.Screen name="index"
+        options={{
+          title: 'Adan Vivero',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen name="about"
+      options={{
+        title: 'About',
+        tabBarIcon: ({ color, focused }) => (
+          <TabBarIcon name={focused ? 'information-circle' : 'information-circle-outline'} color />
+        ),
+      }}
+      />
+      <Tabs.Screen name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
